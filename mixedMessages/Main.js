da2da1562f312f720b6af109aeadf1bb0d59dfe0
unknown <danielwgreen0@gmail.com>
2021-12-06 16:31:19 +0000

    First Commit

---
+++ mixedMessages/Main.js
@@ -0,0 +1,2 @@
+const options1 = ['you will die', 'you will get married', 'you will win the lottery', 'you will experince an unexpected surprise']
+console.log(`There is a ${Math.floor(Math.random()*100)} % chance that ${options1[Math.floor(Math.random() * 4 )]} in around ${Math.floor(Math.random()*365 )} Days`)
